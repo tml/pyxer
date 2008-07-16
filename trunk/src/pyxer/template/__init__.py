0d9f546ad04c69801262ffd45616be2da7247552
dirk.holtwick <dirk.holtwick@95e4eea8-1f52-0410-9f54-838c103a7f1b>
2008-07-16 16:22:01 +0000

    Genshi does not work and maybe will never do so. Use own templating language based on HTML5lib

---
+++ trunk/src/pyxer/template/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+#############################################
+## (C)opyright by Dirk Holtwick, 2008      ##
+## All rights reserved                     ##
+#############################################
+
+__version__ = "$Revision: 103 $"
+__author__  = "$Author: holtwick $"
+__date__    = "$Date: 2007-10-31 17:08:54 +0100 (Mi, 31 Okt 2007) $"
+
+from pyxer.template.template import *
