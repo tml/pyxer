8256329871e70fe25c6ff2fefd2595f06ced8b2d
dirk.holtwick <dirk.holtwick@95e4eea8-1f52-0410-9f54-838c103a7f1b>
2008-08-04 21:34:47 +0000

    Modifications for using Kid in the examples

---
+++ example/public/kid/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+#############################################
+## (C)opyright by Dirk Holtwick, 2008      ##
+## All rights reserved                     ##
+#############################################
+
+from pyxer.base import *
+
+import logging
+log = logging.getLogger(__name__)
+
+@controller
+def index():
+    c.title = "Hello World"
