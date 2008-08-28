08bb011db689ca2ded3ae132e46cdcb9b86c6659
dirk.holtwick <dirk.holtwick@95e4eea8-1f52-0410-9f54-838c103a7f1b>
2008-08-28 13:26:59 +0000

    Fully working; Genshi templating is now default

--- example/public/directory/__init__.py
+++ example/public/directory/__init__.py
@@ -3,7 +3,7 @@
 from pyxer.base import *
 
 import os
-import os.path 
+import os.path
 
 @controller
 def index():
@@ -17,8 +17,8 @@ def index():
 @controller
 def show():
     c.file = req.params.get("file", os.path.dirname(__file__))
-    c.code = open(c.file, "rb").read()    
-    try: 
+    c.code = unicode(open(c.file, "rb").read(), "utf8")
+    try:
         from pygments import highlight
         from pygments.lexers import get_lexer_for_filename
         from pygments.formatters import HtmlFormatter
@@ -27,6 +27,7 @@ def show():
         c.pretty = highlight(c.code, lexer, formatter)
         c.css = HtmlFormatter().get_style_defs('.highlight')
     except:
+        log.exception("Pygments failed?")
         c.pretty = None
         c.css = ""
-    log.debug("Code: %r", c.css)   
+    log.debug("Code: %r", c.css)
