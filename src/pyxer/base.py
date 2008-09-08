a1927b37143d00de7c38c309c45b61fc1a32c8cf
dirk.holtwick <dirk.holtwick@95e4eea8-1f52-0410-9f54-838c103a7f1b>
2008-09-08 18:06:04 +0000

    Started to implement more sophisticated routing

--- src/pyxer/base.py
+++ src/pyxer/base.py
@@ -25,6 +25,7 @@ from pyxer.utils.jsonhelper import json
 from pyxer.controller import \
     Controller, isController, c, g, h, config, \
     session, response, request, resp, req
+from pyxer.routing import Router
 
 import logging
 log = logging.getLogger(__file__)
@@ -34,7 +35,7 @@ def url(url):
     return request.relative_url(url)
 
 def redirect(location, code = 301):
-    " Redirect to other page "   
+    " Redirect to other page "
     # .exeception for Python 2.3 compatibility
     raise exc.HTTPMovedPermanently(location = url(location)).exception
 
@@ -44,34 +45,34 @@ def abort(code = 404):
     raise exc.HTTPNotFound().exception
 
 class StreamTemplateManager:
-    
+
     cache = {}
-    
-    def __init__(self, root):    
+
+    def __init__(self, root):
         self.root = root
-    
-    def load(self, path):        
-        import pyxer.template as pyxer_template 
-        pyxer_template = reload(pyxer_template)        
+
+    def load(self, path):
+        import pyxer.template as pyxer_template
+        pyxer_template = reload(pyxer_template)
         path = os.path.abspath(os.path.join(self.root, path))
         # Test if it is in cache and return if found
         mtime = os.path.getmtime(path)
-        if 0 and self.cache.has_key(path):            
-            template, last = self.cache.get(path)            
-            if mtime <= last:                
+        if 0 and self.cache.has_key(path):
+            template, last = self.cache.get(path)
+            if mtime <= last:
                 return template
             else:
                 log.debug("Found a newer file than the one in the cache for %r", path)
-        # Load the template       
+        # Load the template
         log.debug("Loading template %r in KidTemplateManager", path)
         template = pyxer_template.TemplateSoup(
-            file(path, "r").read())       
-        template.load = self.load         
+            file(path, "r").read())
+        template.load = self.load
         self.cache[path] = (template, mtime)
-        return template      
+        return template
 
 def template_stream(name = None):
-    " Get the template "    
+    " Get the template "
     # XXX What to do with dirname? Scenarios?
     # XXX What to do with absolute url /like/this?
     path = request.template_url
@@ -80,13 +81,13 @@ def template_stream(name = None):
         path = os.path.join(dirname, name)
         dirname = os.path.dirname(path)
     log.debug("Loading template %r", path)
-    soup_manager = StreamTemplateManager(dirname)    
+    soup_manager = StreamTemplateManager(dirname)
     return soup_manager.load(path)
 
 template = template_default = template_stream
 
 def render_stream(*kw):
-    template = template_stream()    
+    template = template_stream()
     template.generate(Dict(c = c, h = Dict(
         url = url,
         redirect = redirect
@@ -98,20 +99,20 @@ render_default = render_stream
 def render_json():
     " Render output as JSON object "
     response.headers['Content-Type'] = 'application/json'
-    result = json(request.result)    
+    result = json(request.result)
     # log.debug("JSON: %r", result)
     return result
 
 class controller(Controller):
- 
+
     def render(self, result, render = None, **kw):
 
         log.debug("Render called with %r %r %r", repr(result)[:40], render, kw)
         # log.debug("Render called with %r %r", render, kw)
-        
+
         # Choose a renderer
         render_func = None
-        
+
         # Render is explicitly defined by @controller
         if render is not None:
             render_func = render
@@ -120,18 +121,18 @@ class controller(Controller):
         # then apply the corresponding template
         # XXX Maybe better test if response.body/body_file is also empty
         elif result is None:
-            render_func = render_default                            
+            render_func = render_default
 
         # Consider dict and list as JSON data
         elif isinstance(result, dict) or isinstance(result, list):
             render_func = render_json
-        
+
         # Execute render function
         log.debug("Render func %r", render_func)
-        if render_func is not None:           
-            request.result = result            
+        if render_func is not None:
+            request.result = result
             log.debug("Render with func %r", render_func)
-            result = render_func(**kw)                            
+            result = render_func(**kw)
 
             # Normalize output
             # if (not None) and (not isinstance(result, str)) and (not isinstance(result, str)):
@@ -140,7 +141,7 @@ class controller(Controller):
         return result
 
 class expose(controller):
-    
+
     def call(self, *a, **kw):
         " Add arguments "
         log.debug("Call func with params %r", dict(request.params))
@@ -174,15 +175,15 @@ class Router(object):
         return exc.HTTPNotFound()(environ, start_response)
 
 class Permission(object):
-    
+
     """
     @controller(permission=Permission('read'))
     """
-    
+
     def __init__(self, permission):
         self.permission
-        
+
     def __call__(self, permissions):
         if isinstance(permissions, basestring):
-            permissions = [permissions]        
+            permissions = [permissions]
         return self.permission in permissions
