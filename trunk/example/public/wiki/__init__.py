08905392721868874b3bc8c17a9645e154740d63
dirk.holtwick <dirk.holtwick@95e4eea8-1f52-0410-9f54-838c103a7f1b>
2008-09-08 21:26:16 +0000

    Wiki

---
+++ trunk/example/public/wiki/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: UTF-8 -*-
+
+from pyxer.base import *
+import model
+
+log = logging.getLogger(__name__)
+
+from pyxer.route import *
+
+router = Router(__name__)
+router.add("^content\/(?P<name>.*?)", action=index)
+
+@controller
+def index():
+
+    # Default values
+    c.entry = dict(
+        title = "First Page",
+        body = "Enter your content here"
+        )
+
+    # Get data from database
+    result = model.Wiki.query.all()
+    if result:
+        c.entry = result[0]
+
+@expose
+def commit(title, body):
+    """
+    Write form content into database and jump to index page
+    to avoid that a reload of the page creates a duplicate entry.
+    """
+
+    log.debug("Wiki entry %r %r", title, body)
+
+    # Using Elixir
+    model.Wiki(title=title, body=body)
+    model.commit()
+
+    # Redirect to index
+    redirect(".")
