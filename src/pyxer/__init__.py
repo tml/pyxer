5fe082ab9f85f52809fe8a372fa89dbece12a7a6
dirk.holtwick <dirk.holtwick@95e4eea8-1f52-0410-9f54-838c103a7f1b>
2009-05-30 10:43:00 +0000

    Next version info

--- src/pyxer/__init__.py
+++ src/pyxer/__init__.py
@@ -6,8 +6,8 @@
 
 __author__ = "$Author: holtwick $"
 __date__ = "$Date: 2007-10-31 17:08:54 +0100 (Mi, 31 Okt 2007) $"
-__version__ = VERSION = "VERSION{0.7.1}VERSION"[8: - 8]
-__build__ = BUILD = "BUILD{2009-05-20}BUILD"[6: - 6]
+__version__ = VERSION = "VERSION{0.7.2}VERSION"[8: - 8]
+__build__ = BUILD = "BUILD{2009-05-30}BUILD"[6: - 6]
 
 VERSION_STR = "Pyxer %s (Build %s)\n(c) Dirk Holtwick <dirk.holtwick@gmail.com>, Germany\nWebsite http://www.pyxer.net" % (
     __version__,
